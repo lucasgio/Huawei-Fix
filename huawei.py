0599ea57b78a0f5b15b36a580df3c54662d2c328
lucasgio <mikaelosoltes@gmail.com>
2020-12-31 05:51:08 -0500

    Final Release

--- huawei.py
+++ huawei.py
@@ -6,88 +6,106 @@ import os.path
 from io import open
 import time
 
+class Huawei:
 
-# Instanciamos el objeto
-huawei = Tk()
+    def __init__(self):
+        self.huawei = Tk()
+        self.titulo = 'HUAWEI FIX CUBACEL'
+        self.icono = os.path.abspath('.\\imagenes\\logo.ico')
+        self.size = '700x650'
 
-# Creamos las dimensiones de la aplicacion
-huawei.geometry('700x600')
 
-# Movimiento de las dimensiones default(False)
-huawei.resizable(0,0)
+    def config(self):
+        # Creamos las dimensiones de la aplicacion
+        self.huawei.geometry(self.size)
+        # Movimiento de las dimensiones default(False)
+        self.huawei.resizable(0,0)
+        # Titulo de aplicacion
+        self.huawei.title(self.titulo)
+        # Icono
+        self.huawei.iconbitmap(self.icono)
 
-# Titulo de aplicacion
-huawei.title('HUAWEI FIX METODO SIM CUBACEL')
-# Funcion para mostrar mensaje de informacion
-def msjAyuda():
-    msg.showinfo('Informacion',"""
-        Este metodo no es para hacer unlock a ningun huawei
-        Funcional solamente para eliminar el reinicio constante
-        en algunos modelos de Huawei
-    """)
-# Funcion con la logica del procedimiento
-def remover():
-    try:
-        adb = ParallelADB.run('adb device')
-        #adb = os.system('adb devices')
-        print(adb)
-        if adb == 0 :
-            Label(frameizquierdo,text='Intentando remover.....').pack()
-            adb = ParallelADB.run('adb shell')
-            adb = ParallelADB.run('pm list package')
-            adb = ParallelADB.run('pm uninstall -k —user 0 com.huawei.tmecustomize')
-            print(adb)
-        elif adb == 1:
-            msg.showerror('Error',"""
-                1.Verifique el cable usb este conectado la pc
-                2.Verifique la depuracion usb este activa
-                3.Verifique que SIM este extraida correctamente
-            """)
-    except Exception:
-        msg.showerror('Error',"""
-            Ha ocurrido un error, por favor intente de nuevo
+    # Funcion para mostrar mensaje de informacion
+    def msjAyuda(self):
+        msg.showinfo('Informacion',"""
+            Este metodo soluciona el reinicio constante en 
+            algunos telefonos huawei que despues de un update 
+            aparece un popup exigiendo reinicio constante.
+
+            NO es para hacer unlock permanente
         """)
 
-# Primer Frame
-frameizquierdo = LabelFrame(huawei,width=400,height=600)
-frameizquierdo.pack(side=LEFT)
-frameizquierdo.pack_propagate(False)
-
-# Contenido del primer frame
-    # Texto e Instruciones
-subtitulo = Label(frameizquierdo,text='METODO FIX HUAWEI SIM CUBACEL')
-subtitulo.pack(pady=20)
-Label(frameizquierdo,text='Instrucciones').pack(anchor=W)
-Label(frameizquierdo,text='1.Habilitar a depuracion usb').pack(anchor=W)
-Label(frameizquierdo,text='2.Retirar la SIM CUBACEL').pack(anchor=W)
-   # Boton de ayuda
-ayuda = Button(frameizquierdo,text='Ayuda',command=msjAyuda)
-ayuda.config(padx=50)
-ayuda.pack(side=LEFT)
-   # Boton de unlock
-doit = Button(frameizquierdo,text='Remover',command=remover)   
-doit.config(padx=50)
-doit.pack(side=LEFT)
 
 
+    def framederecho(self):
+        framederecho = Frame(self.huawei,width=350,height=650)
+        framederecho.pack(anchor=W)
+        # Subtitulo de la aplicacion
+        subtitulo = Label(framederecho,text='HUAWEI FIX SIM CUBACEL')
+        subtitulo.config(padx=50,pady=30)
+        subtitulo.pack()
+        # Intrucciones
+        Label(framederecho,text='Instruccciones').pack(anchor=W,padx=20)
+        Label(framederecho,text='1.Habilitar depuracion usb').pack(anchor=W,padx=20)
+        Label(framederecho,text='2.Remover SIM CUBACEL').pack(anchor=W,padx=20)
+        Label(framederecho,text='3.Presionar boton FIX').pack(anchor=W,padx=20)
+        # Botones
+        ayuda = Button(framederecho,text='Consejos',command=self.msjAyuda)
+        ayuda.config(padx=20)
+        ayuda.pack(side=LEFT,padx=20,pady=50)
+        fix = Button(framederecho,text='FIX',command=self.remover)
+        fix.config(padx=20)
+        fix.pack(side=LEFT,)
 
-# Segundo Frame
-framederecho = LabelFrame(huawei,width=350,height=600)
-framederecho.config(bg='green')
-framederecho.pack(side=RIGHT)
-framederecho.pack_propagate(False)
-huaweimg = Image.open('./imagenes/huawei.png')
-img = ImageTk.PhotoImage(huaweimg)
-Label(framederecho,image=img).pack()
+    # Funcion con la logica del procedimiento
+    def remover(self):
+        try:
+            adb = os.system('adb devices')
+            #print(adb)
+            if adb == 1 :
+                Label(self.huawei,text='Intentando remover.....').pack(side=BOTTOM)
+                adb = os.system('adb shell')
+                adb = os.system('pm list package')
+                adb = os.system('pm uninstall -k —user 0 com.huawei.tmecustomize')
+                #print(adb)
+                self.msgsucces()
+            elif adb == 0:
+                msg.showerror('Error',"""
+                    1.Verifique el cable usb este conectado la pc
+                    2.Verifique la depuracion usb este activa
+                    3.Verifique que SIM este extraida correctamente
+                    """
+                )
+        except Exception:
+            msg.showerror('Error',"""
+                Ha ocurrido un error, por favor intente de nuevo
+                """
+            )
+  
+    def msgsucces(self):
+        msg.showinfo("Exito","""
+            Ya fue removido exitosamente
+            Por favor,reinicie e inserte la sim.
 
+            Gracias
+        """)
 
+    def msgwelcome(self):
+        msg.showinfo('Info',"""
+            Este software esta realizado para distribucion gratuita
+            Es un metodo que esta en la web solo fue 
+            desarrollado para automatizar el proceso
 
+            Desarrollado por Iosvany
+            2021
 
+        """)
 
+    def run(self):
+        self.huawei.mainloop()
 
-
-
-
-
-huawei.mainloop()
-
+newhuawei = Huawei()
+newhuawei.config()
+newhuawei.framederecho()
+newhuawei.msgwelcome()
+newhuawei.run()
